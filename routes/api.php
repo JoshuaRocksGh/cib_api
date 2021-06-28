5c5e6b7569f2ca1775e30aec3bf1bee088f31711
Joshua Tetteh <josh.tetteh01@gmail.com>
2021-06-28 17:04:36 +0000

    approval update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Accounts\AccountController;
+use App\Http\Controllers\ApprovalRequestController;
+use App\Http\Controllers\ApprovedRequestController;
 use App\Http\Controllers\BULK\BulkTransferController;
 use App\Http\Controllers\BULK\ImportExcelController;
 use App\Http\Controllers\PendingRequestController;
@@ -8,6 +10,7 @@ use App\Http\Controllers\RequestController;
 use App\Http\Controllers\Requests\StatementRequestGoForPendingController;
 use App\Http\Controllers\Transfers\OwnAccountGoForPendingController;
 use App\Http\Controllers\Transfers\SameBankGoForPendingController;
+// use App\Http\Controllers\ApprovalRequestController;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -36,5 +39,7 @@ Route::post('/statement-request-gone-for-pending', [StatementRequestGoForPending
 Route::get('/get-bulk-upload-list', [BulkTransferController::class, 'get_bulk_upload_list'])->name('get-bulk-upload-list');
 Route::get('/get-bulk-upload-detail-list-api', [BulkTransferController::class, 'get_bulk_upload_detail_list_api'])->name('get-bulk-upload-detail-list-api');
 Route::get('/pending-request-api' , [RequestController::class, 'all_approval_request'])->name('pending-request-api');
+Route::post('/approve-request-api', [ApprovalRequestController::class, 'approve_request'])->name('approve-request-api');
+Route::post('/approved-request-api', [ApprovedRequestController::class, 'approved_request'])->name('approved-request-api');
 
 Route::post('/account/getAccounts', [AccountController::class, 'get_accounts'])->name('getAccounts');
